a58ef0e4f588cae668d42a4e10091ff0eba1c9b0
katarzyna tobis <katarzyna@kinside.com>
2021-07-15 21:25:02 +0200

    - romove router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
   router: {
-    base: 'katarzynatobis'
+    base: '/katarzynatobis/'
   },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
