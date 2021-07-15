a2541669b627ac7e917388eafd40327c62940106
katarzyna tobis <katarzyna@kinside.com>
2021-07-15 12:18:56 +0200

    + cv section

--- components/cvSection.vue
+++ components/cvSection.vue
@@ -1,15 +1,28 @@
 <template>
-  <section>
+  <section class="cv-section">
     <header>
-      <h2 />
+      <h2><slot name="title" /></h2>
     </header>
+    <slot />
   </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import Vue from 'vue'
 
-export default defineComponent({
+export default Vue.extend({
 
 })
 </script>
+
+<style lang="postcss" scoped>
+.cv-section {
+  margin: calc(var(--unit) * 10) 0;
+
+  h2 {
+    font-family: var(--serif);
+    font-size: calc(var(--paragraph-size) * 1.75);
+    margin-bottom: calc(var(--unit) * 8);
+  }
+}
+</style>
