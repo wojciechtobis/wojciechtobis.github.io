ee7c03b2c0dd7a2c1950e7c363aac08dcb396241
katarzyna tobis <katarzyna@kinside.com>
2021-07-15 13:27:46 +0200

    + cv aside v1

--- components/cv.vue
+++ components/cv.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <cv-header />
-    <cv-main />
+  <div class="cv">
+    <div>
+      <cv-header />
+      <cv-main />
+    </div>
+    <cv-aside />
   </div>
 </template>
 
@@ -9,11 +12,31 @@
 import Vue from 'vue'
 import CvHeader from './cvHeader.vue'
 import CvMain from './cvMain.vue'
+import CvAside from './cvAside.vue'
 
 export default Vue.extend({
   components: {
     CvHeader,
-    CvMain
+    CvMain,
+    CvAside
   }
 })
 </script>
+
+<style lang="postcss" scoped>
+.cv {
+  --cv-width: calc(var(--unit) * 250);
+
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+  width: var(--cv-width);
+  min-width: var(--cv-width);
+  margin: calc(var(--unit) * 25) auto;
+  background: white;
+
+  & > *:first-child {
+    padding: calc(var(--unit) * 25);
+    padding-right: calc(var(--unit) * 15);
+  }
+}
+</style>
---
+++ components/cvAside.vue
@@ -0,0 +1,33 @@
+<template>
+  <aside>
+    <div v-for="item in items" :key="item.title">
+      {{ item.title }}
+      {{ item.description }}
+    </div>
+  </aside>
+</template>
+
+<script>
+import Vue from 'vue'
+import { markRaw } from '@nuxtjs/composition-api'
+
+export default Vue.extend({
+  setup () {
+    const items = markRaw([
+      {
+        title: 'title',
+        description: 'djfkkfk description description'
+      }
+    ])
+    return {
+      items
+    }
+  }
+})
+</script>
+
+<style scoped>
+aside {
+  border: 1px solid red;
+}
+</style>
